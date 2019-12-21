80c55078f46fd73053e3a7fa1bf76065fac69129
bruce-qiong <270064522@qq.com>
2019-12-21 20:27:44 +0800

    Add files via upload

--- JDK_auto_install.sh
+++ JDK_auto_install.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+mkdir -p /usr/local/JDK8
+tar	-xf jdk-8u231-linux-x64.tar.gz -C  /usr/local/JDK8
+echo -e 'export JAVA_HOME=/usr/local/JDK8/jdk1.8.0_231/\nexport JRE_HOME=${JAVA_HOME}/jre\nexport CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib:$CLASSPATH\nexport JAVA_PATH=${JAVA_HOME}/bin:${JRE_HOME}/bin\nexport PATH=$PATH:${JAVA_PATH}' >>/etc/profile
+source /etc/profile	
+java -version
 
